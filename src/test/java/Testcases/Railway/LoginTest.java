93b08dae367ddfbff1ea46f834503675fbe58491
nguyenbinv <nguyenvanhuy1999@gmail.com>
2021-06-16 17:06:33 +0700

    Update login testcases

--- src/test/java/Testcases/Railway/LoginTest.java
+++ src/test/java/Testcases/Railway/LoginTest.java
@@ -10,34 +10,33 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class LoginTest {
-    @BeforeTest
-    public void beforeMethod(){
-        System.out.println("Pre-Condition");
-        System.setProperty("webdriver.chrome.driver", Utilities.getProjectPath() +
-                "/src/test/java/Executables/chromedriver.exe");
-        Constant.WEBDRIVER = new ChromeDriver();
-        Constant.WEBDRIVER.manage().window().maximize();
-    }
-
-    @AfterMethod
-    public void afterMethod(){
-        System.out.println("Post-Condition");
-        Constant.WEBDRIVER.quit();
-    }
+public class LoginTest extends BaseTest {
+    private HomePage homePage = new HomePage();
+    private LoginPage loginPage = new LoginPage();
 
     @Test
-    public void TC01(){
+    public void TC01() {
         System.out.println("TC01 - User can log into Railway with valid username and password");
-        HomePage homePage = new HomePage();
+
         homePage.open();
 
-        LoginPage loginPage = homePage.gotoLoginPage();
+        homePage.gotoLoginPage();
 
         String actualMsg = loginPage.login(Constant.USERNAME, Constant.PASSWORD).getWelcomeMessage();
         String expectedMsg = "Welcome " + Constant.USERNAME;
 
         Assert.assertEquals(actualMsg, expectedMsg, "Welcome message is not displayed as expected");
+    }
+
+    @Test
+    public void TC02(){
+        System.out.println("TC02 - User can't login with blank \"Username\" textbox");
+
+        homePage.open();
+
+        homePage.gotoLoginPage();
+
+        String actualMsg = loginPage.login("", Constant.PASSWORD).
     }
 
 }
